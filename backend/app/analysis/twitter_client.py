192e7832594371e39c337d6aea11c0ca850e6499
ac-hj <hyeon.j.choi00@gmail.com>
2022-12-12 18:16:11 -0500

    set up tweepy query & basic sentiment analysis

--- backend/app/analysis/twitter_client.py
+++ backend/app/analysis/twitter_client.py
@@ -8,6 +8,11 @@ Referenced tutorials to set up authentication here:
 
 import configparser
 import tweepy
+from flair.models import TextClassifier
+from flair.data import Sentence
+from flair.models import SequenceTagger
+import re
+
 
 class TwitterClient(object):
   """
@@ -21,17 +26,16 @@ class TwitterClient(object):
     self.read_config()
 
     ### ONLY AVAILABLE FOR ELEVATED ACCESS ###
-    # auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-    # auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    # api = tweepy.API(auth)
-    # return api
+    auth = tweepy.OAuthHandler(self.ELEVATED_KEYS['api_key'], self.ELEVATED_KEYS['api_key_secret'])
+    auth.set_access_token(self.ELEVATED_KEYS['api_access_token'], self.ELEVATED_KEYS['api_access_token_secret'])
+    self.api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    # try: 
-    self.client = tweepy.Client(
-    consumer_key=API_KEY,
-    consumer_secret=API_KEY_SECRET,
-    access_token=ACCESS_TOKEN,
-    access_token_secret=ACCESS_TOKEN_SECRET)
+    ### ESSENTIAL ACCESS VERSION ### 
+    # self.client = tweepy.Client(
+    # consumer_key=API_KEY,
+    # consumer_secret=API_KEY_SECRET,
+    # access_token=ACCESS_TOKEN,
+    # access_token_secret=ACCESS_TOKEN_SECRET)
 
     # except tweepy.TweepError as e:
     #   # print error (if any)
@@ -45,42 +49,85 @@ class TwitterClient(object):
       config = configparser.ConfigParser()
       config.read('../../config.ini')
 
-      global API_KEY
-      global API_KEY_SECRET
-      global ACCESS_TOKEN
-      global ACCESS_TOKEN_SECRET
+      self.ESSENTIAL_KEYS = {
+        'api_key': config['twitter-essential']['api_key'],
+        'api_key_secret': config['twitter-essential']['api_key_secret'],
+        'api_access_token': config['twitter-essential']['access_token'],
+        'api_access_token_secret': config['twitter-essential']['access_token_secret']
+      }
 
-      print(config['twitter'])
-      
-      API_KEY = config['twitter']['api_key']
-      API_KEY_SECRET = config['twitter']['api_key_secret']
-
-      ACCESS_TOKEN = config['twitter']['access_token']
-      ACCESS_TOKEN_SECRET = config['twitter']['access_token_secret']
+      self.ELEVATED_KEYS = {
+        'api_key': config['twitter-elevated']['api_key'],
+        'api_key_secret': config['twitter-elevated']['api_key_secret'],
+        'api_access_token': config['twitter-elevated']['access_token'],
+        'api_access_token_secret': config['twitter-elevated']['access_token_secret']
+      }
 
   def get_tweets(self):
     """
       API call to get tweets based on query keyword(s)
     """
 
-    query = 'Elon Musk'
+    query = '#gme'
 
     # try:
-    tweets = self.client.search_recent_tweets(query=query, max_results=10, user_auth=True)
-    print(tweets)
-    for tweet in tweets:
-      print(tweet)
+    public_tweets = [status for status in tweepy.Cursor(self.api.search_tweets, q=query).items(100)]
+    return public_tweets
     # except tweepy.TweepError as e:
     #     # print error (if any)
     #     print("Error : " + str(e))
 
+'''
+  For a tweet, return get sentiment score through Flair API
+  Input: tweet str
+  Return: score float
+'''
+def get_sentiment(tweet):
+  # > Referenced: https://towardsdatascience.com/text-classification-with-state-of-the-art-nlp-library-flair-b541d7add21f?gi=3675966b8dff
+  classifier = TextClassifier.load('en-sentiment')
+  sentence = Sentence(tweet)
+  classifier.predict(sentence)
+  # print('Sentence above is: ', sentence.labels)
+  print(tweet)
+  print(sentence.labels[0].to_dict()['value'])
+  print(sentence.labels[0].to_dict()['confidence'])
+  
+  # stacked_embeddings.embed(text)
+  # classifier.predict(text)
+  # value = text.labels[0].to_dict()['value'] 
+  # if value == 'POSITIVE':
+  #     result = text.to_dict()['labels'][0]['confidence']
+  # else:
+  #     result = -(text.to_dict()['labels'][0]['confidence'])
+      
+  # return round(result, 3)
+  
+def clean(raw):
+    """ Remove hyperlinks and markup """
+    result = re.sub("<[a][^>]*>(.+?)</[a]>", 'Link.', raw)
+    result = re.sub('&gt;', "", result)
+    result = re.sub('&#x27;', "'", result)
+    result = re.sub('&quot;', '"', result)
+    result = re.sub('&#x2F;', ' ', result)
+    result = re.sub('<p>', ' ', result)
+    result = re.sub('</i>', '', result)
+    result = re.sub('&#62;', '', result)
+    result = re.sub('<i>', ' ', result)
+    result = re.sub("\n", '', result)
+    return result
+    
 if __name__ == '__main__':
-  ### ONLY AVAILABLE FOR ELEVATED ACCESS ###
-  # Create API object
-  # api = connect_to_twitter_OAuth()
-  # public_tweets = api.home_timeline()
-  # print(public_tweets)
-
-  ### ESSENTIAL ACCESS VERSION ###
   client = TwitterClient()
-  client.get_tweets()
+  public_tweets = client.get_tweets()
+  print(len(public_tweets))
+  # get_sentiment("I am happy")
+
+  for i in range(2):
+    print(public_tweets[i].text)
+    print(clean(public_tweets[i].text))
+    get_sentiment(public_tweets[i].text)
+
+  # for i in range(2):
+  #   print("sentiment score: ", get_sentiment(public_tweets[i]), public_tweets[i])
+
+
