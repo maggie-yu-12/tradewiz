d11ffbc06348df4a6f74ee7471062ff476834d69
ac-hj <hyeon.j.choi00@gmail.com>
2022-12-30 12:49:15 -0800

    clean up backend & set up eslint

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,7 @@ from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, origins=[
-  'http://localhost:1234',
-])
+cors = CORS(app, origins=['*'])
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True)
 
 from app import views
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,28 +5,30 @@ import './App.css';
 
 export function App() {
 
-   // new line start
+  // new line start
   const [profileData, setProfileData] = useState(null)
   axios.defaults.baseURL = 'http://localhost:8000';
 
   function getData() {
     axios({
       method: "GET",
-      url:"/profile",
+      url: "/profile",
     })
-    .then((response) => {
-      const res =response.data
-      setProfileData(({
-        profile_name: res.name,
-        about_me: res.about}))
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
+      .then((response) => {
+        const res = response.data
+        setProfileData(({
+          profile_name: res.name,
+          about_me: res.about
+        }))
+      }).catch((error) => {
+        if (error.response) {
+          console.log(error.response)
+          console.log(error.response.status)
+          console.log(error.response.headers)
         }
-    })}
-    //end of new line 
+      })
+  }
+  //end of new line 
 
   return (
     <div className="App">
@@ -47,11 +49,11 @@ export function App() {
         {/* new line start*/}
         <p>To get your profile details: </p><button onClick={getData}>Click me</button>
         {profileData && <div>
-              <p>Profile name: {profileData.profile_name}</p>
-              <p>About me: {profileData.about_me}</p>
-            </div>
+          <p>Profile name: {profileData.profile_name}</p>
+          <p>About me: {profileData.about_me}</p>
+        </div>
         }
-         {/* end of new line */}
+        {/* end of new line */}
       </header>
     </div>
   );
