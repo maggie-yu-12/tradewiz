7fa9aacb168c874a05a4607051a3572e5a13a701
ac-hj <hyeon.j.choi00@gmail.com>
2023-01-06 20:52:50 -0800

    add simple table w sample data

---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,86 @@
+import { useMemo } from 'react';
+
+import {
+  Box
+} from '@mui/material';
+import MaterialReactTable from 'material-react-table';
+
+import sample_data from '../../model/sample_data.json';
+
+import '../../styles/home.css';
+
+const Home = () => {
+  const data = useMemo(() => sample_data, [])
+  const column_headers = useMemo(() => ["company", "symbol", "week", "month", "sentiment"])
+  const Sentiment = Object.freeze({
+    POSITIVE: Symbol("positive"),
+    NEGATIVE: Symbol("negative"),
+  });
+
+  // {
+  //   accessorKey: 'company', //simple recommended way to define a column
+  //   header: 'Company',
+  //   muiTableHeadCellProps: { sx: { color: 'blue' } }, //custom props
+  // },
+  const columns = useMemo(
+    () => column_headers.map((header) => ({
+      accessorFn: (originalRow) => getColumnAccessor(originalRow, header),
+      id: header,
+      header: header,
+      Header: <i style={{ color: 'blue' }}>{header}</i>,
+      Cell: ({ cell }) => header === "sentiment" ? (
+        <Box
+          sx={(theme) => ({
+            backgroundColor:
+              cell.getValue() === Sentiment.POSITIVE.description
+                ? theme.palette.success.dark :
+                theme.palette.warning.dark,
+            borderRadius: '0.25rem',
+            color: '#fff',
+            maxWidth: '9ch',
+            textAlign: 'center',
+            p: '0.25rem',
+          })}
+        >
+          {cell.getValue()}
+        </Box>
+      ) : undefined
+    }))
+  )
+
+  const getColumnAccessor = (originalRow, header) => {
+    switch (header) {
+      case "company":
+        return originalRow.company
+      case "symbol":
+        return originalRow.symbol
+      case "week":
+        return originalRow.week
+      case "month":
+        return originalRow.month
+      case "sentiment":
+        return originalRow.sentiment
+    }
+  }
+
+  return (
+    <div className='home-container'>
+      <MaterialReactTable
+        columns={columns}
+        data={data}
+        // enableRowSelection //enable some features
+        enableColumnOrdering
+        enableGlobalFilter={false} //turn off a feature
+        enableRowNumbers
+        enableRowActions
+        muiSearchTextFieldProps={{
+          placeholder: 'Search Stock',
+          sx: { minWidth: '18rem' },
+          variant: 'outlined',
+        }}
+      />
+    </div>
+  )
+}
+
+export default Home
