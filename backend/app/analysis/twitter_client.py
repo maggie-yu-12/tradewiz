76f40659d1b725c59061c0253fe60f492b7bc6ac
Yuchen Fan <alexfan@Alexs-MacBook-Pro-3.local>
2023-02-12 22:24:33 -0500

    changes

--- backend/app/analysis/twitter_client.py
+++ backend/app/analysis/twitter_client.py
@@ -1,40 +1,40 @@
 """
-
 Referenced tutorials to set up authentication here: 
   > https://medium.com/mlearning-ai/how-to-get-tweets-by-python-and-twitter-api-2022-a440c635c3ac
   > https://medium.com/mlearning-ai/how-to-get-tweets-by-python-and-twitter-api-2022-a440c635c3ac
-
 """
 
-# pylint: disable=E1136
 
 import configparser
 import json
 import re
+import string
+from itertools import repeat
+from multiprocessing import Pool
 
 import requests
 import tweepy
+import yfinance as yf
 from flair.data import Sentence
 from flair.models import SequenceTagger, TextClassifier
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+
+import nltk;nltk.download('stopwords')
 
 
 class TwitterClient(object):
-    """
+    '''
       Twitter client class for sentiment analysis
-    """
+    '''
+    ## CLASS VARIABLES ##
+    # instantiate classifier
+    classifier = TextClassifier.load('en-sentiment')
 
     def __init__(self):
-        """
-          Set up authentication and initialize client
-        """
-        self.read_config()
-
-        ### ONLY AVAILABLE FOR ELEVATED ACCESS ###
-        auth = tweepy.OAuthHandler(
-            self.ELEVATED_KEYS['api_key'], self.ELEVATED_KEYS['api_key_secret'])
-        auth.set_access_token(
-            self.ELEVATED_KEYS['api_access_token'], self.ELEVATED_KEYS['api_access_token_secret'])
-        self.api = tweepy.API(auth, wait_on_rate_limit=True)
+        #TwitterClient.setup_config()
+        self.setup_config()
 
         ### ESSENTIAL ACCESS VERSION ###
         # self.client = tweepy.Client(
@@ -47,114 +47,207 @@ class TwitterClient(object):
         #   # print error (if any)
         #   print("Error : " + str(e))
 
+    #@classmethod
     def read_config(self):
-        """
+        '''
           Retrieve authentication keys from config file and initialize
-        """
+        '''
 
-        config = configparser.ConfigParser()
+        config = configparser.RawConfigParser()
         config.read('../../config.ini')
 
-        self.ESSENTIAL_KEYS = {
-            'api_key': config['twitter-essential']['api_key'],
-            'api_key_secret': config['twitter-essential']['api_key_secret'],
-            'api_access_token': config['twitter-essential']['access_token'],
-            'api_access_token_secret': config['twitter-essential']['access_token_secret'],
-            'api_bearer_token': config['twitter-essential']['bearer_token'],
-        }
+        #self.ESSENTIAL_KEYS = {
+            #'api_key': config['twitter-essential']['api_key'],
+            #'api_key_secret': config['twitter-essential']['api_key_secret'],
+            #'api_access_token': config['twitter-essential']['access_token'],
+            #'api_access_token_secret': config['twitter-essential']['access_token_secret'],
+            #'api_bearer_token': config['twitter-essential']['bearer_token'],
+        #}
 
         self.ELEVATED_KEYS = {
             'api_key': config['twitter-elevated']['api_key'],
             'api_key_secret': config['twitter-elevated']['api_key_secret'],
             'api_access_token': config['twitter-elevated']['access_token'],
-            'api_access_token_secret': config['twitter-elevated']['access_token_secret']
+            'api_access_token_secret': config['twitter-elevated']['access_token_secret'],
+            'api_bearer_token': config['twitter-elevated']['bearer_token'],
         }
 
-        # self.BEARER_TOKEN = config['twitter-essential']['bearer_token']
+        self.BEARER_TOKEN = config['twitter-essential']['bearer_token']
 
-    def get_tweets(self):
-        """
-          API call to get tweets based on query keyword(s)
-        """
+    #@classmethod
+    def setup_config(self):
+        '''
+          Set up authentication and initialize client
+        '''
 
-        query = '#gme'
+        self.read_config()
 
-        # try:
-        public_tweets = [status for status in tweepy.Cursor(
-            self.api.search_tweets, q=query).items(100)]
-        return public_tweets
-        # except tweepy.TweepError as e:
-        #     # print error (if any)
-        #     print("Error : " + str(e))
+        ### ONLY AVAILABLE FOR ELEVATED ACCESS ###
+        auth = tweepy.OAuthHandler(
+            self.ESSENTIAL_KEYS['api_key'], self.ESSENTIAL_KEYS['api_key_secret'])
+        auth.set_access_token(
+            self.ESSENTIAL_KEYS['api_access_token'], self.ESSENTIAL_KEYS['api_access_token_secret'])
+        self.api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    def get_old_tweets(self):
-        endpoint = "https://api.twitter.com/1.1/tweets/search/30day/dev.json"
+    #@classmethod
+    def get_old_tweets(self, date_since, date_until, *search_words):
+        '''
+            Queries past tweets based on provided query and date range and writes them to local files based on date range
+            Args:
+                date_since (string): Start date from which we want to query the tweets from
+                date_until (string): End date to which we want to query the tweets until
+                *search_words: Variable argumens for search words (multiple search words are OR'ed by default)
+            Returns: None
+        '''
+        # Create a name for file that we will be writing tweets to
+        filename = "past_tweets_" + date_since + "_" + date_until + ".txt"
+        search_string = ' OR '.join(search_words)
 
-        headers = {"Authorization": {
-            self.ESSENTIAL_KEYS['api_bearer_token']}, "Content-Type": "application/json"}
+        # UNCOMMENT TO RUN THE QUERIES
+        # !! NOTE: WE NEED TO BE CAREFUL BC WE CAN run AT MAX 50 QUERIES PER MONTH :((
+        tweets = tweepy.Cursor(self.api.search_full_archive,
+                                query=search_string,
+                                label="fullArchiveEnviron",
+                                fromDate=date_since,
+                                toDate=date_until
+                                ).items(limit=10)
 
-        data = '{"query":"(snow OR sleet OR hail OR (freezing rain)) has:images", "fromDate": "201802020000", "toDate": "201802240000"}'
+        with open(filename, "w") as file:
+          for elem in tweets:
+                 # Turn Status (`elem` data type) object into a JSON format to make writable to a file
+              s = json.dumps(elem._json)
+              file.write(s)
+        #        file.write("\n")
 
-        response = requests.post(endpoint, data=data, headers=headers).json()
+    #@classmethod
+    def get_sentiment(self, tweet):
+        '''
+            For a tweet, obtains sentiment score through Flair API
+            Args:
+                tweet ([string]): cleaned tokens frm a tweet
+            Returns:
+                score (string): binary sentiment ('POSITIVE' or 'NEGATIVE')
+                confidence (float): confidence level of the result (in range 0-1)
+            
+        '''
+        # > Referenced: https://towardsdatascience.com/text-classification-with-state-of-the-art-nlp-library-flair-b541d7add21f?gi=3675966b8dff
+        sentence = Sentence(tweet)
+        cls.classifier.predict(sentence)
 
-        print(json.dumps(response, indent=2))
+        
+        return sentence.labels[0].to_dict()['value'], sentence.labels[0].to_dict()['confidence']
 
+        # stacked_embeddings.embed(text)
+        # classifier.predict(text)
+        # value = text.labels[0].to_dict()['value']
+        # if value == 'POSITIVE':
+        #     result = text.to_dict()['labels'][0]['confidence']
+        # else:
+        #     result = -(text.to_dict()['labels'][0]['confidence'])
 
-'''
-  For a tweet, return get sentiment score through Flair API
-  Input: tweet str
-  Return: score float
-'''
+        # return round(result, 3)
 
+    #@staticmethod
+    def clean(self, tweet_text):
+        '''
+            Preprocesses/cleans each tweet
+            Args:
+                tweet_text (string): text content ('text' field) extracted from a single tweet object
+            Returns:
+                tweets_clean ([string]): array of cleaned tokens from the inputted tweet
+        '''
+        # Remove old style retweet text "RT"
+        result = re.sub(r'^RT[\s]', '', tweet_text)
+        # Remove links
+        result = re.sub("<[a][^>]*>(.+?)</[a]>", 'Link.', result)
+        # Remove hashtags and other characters
+        result = re.sub(r'#', '', result)
+        result = re.sub('&gt;', "", result)
+        result = re.sub('&#x27;', "'", result)
+        result = re.sub('&quot;', '"', result)
+        result = re.sub('&#x2F;', ' ', result)
+        result = re.sub('<p>', ' ', result)
+        result = re.sub('</i>', '', result)
+        result = re.sub('&#62;', '', result)
+        result = re.sub('<i>', ' ', result)
+        result = re.sub("\n", '', result)
+        result = re.sub(r"(?i)@[a-z0-9_]+", '', result)
 
-def get_sentiment(tweet):
-    # > Referenced: https://towardsdatascience.com/text-classification-with-state-of-the-art-nlp-library-flair-b541d7add21f?gi=3675966b8dff
-    classifier = TextClassifier.load('en-sentiment')
-    sentence = Sentence(tweet)
-    classifier.predict(sentence)
-    # print('Sentence above is: ', sentence.labels)
-    print(tweet)
-    print(sentence.labels[0].to_dict()['value'])
-    print(sentence.labels[0].to_dict()['confidence'])
+        # instantiate tokenizer class
+        tokenizer = TweetTokenizer(preserve_case=False,
+                                strip_handles=True, reduce_len=True)
+        # tokenize tweets
+        tweet_tokens = tokenizer.tokenize(result)
 
-    # stacked_embeddings.embed(text)
-    # classifier.predict(text)
-    # value = text.labels[0].to_dict()['value']
-    # if value == 'POSITIVE':
-    #     result = text.to_dict()['labels'][0]['confidence']
-    # else:
-    #     result = -(text.to_dict()['labels'][0]['confidence'])
+        # Import the english stop words list from NLTK
+        stopwords_english = stopwords.words('english')
 
-    # return round(result, 3)
+        # Creating a list of words without stopwords
+        tweets_clean = []
+        for word in tweet_tokens:
+            if word not in stopwords_english and word not in string.punctuation:
+                tweets_clean.append(word)
 
-
-def clean(raw):
-    """ Remove hyperlinks and markup """
-    result = re.sub("<[a][^>]*>(.+?)</[a]>", 'Link.', raw)
-    result = re.sub('&gt;', "", result)
-    result = re.sub('&#x27;', "'", result)
-    result = re.sub('&quot;', '"', result)
-    result = re.sub('&#x2F;', ' ', result)
-    result = re.sub('<p>', ' ', result)
-    result = re.sub('</i>', '', result)
-    result = re.sub('&#62;', '', result)
-    result = re.sub('<i>', ' ', result)
-    result = re.sub("\n", '', result)
-    return result
+        return tweets_clean
 
 
 if __name__ == '__main__':
     client = TwitterClient()
-    public_tweets = client.get_tweets()
-    print(len(public_tweets))
+    #client.read_config()
+    #client.setup_config()
+    #public_tweets = client.get_tweets()
+    #print(len(public_tweets))
     # get_sentiment("I am happy")
+
+
 
     # for i in range(2):
     #   print(public_tweets[i].text)
     #   print(clean(public_tweets[i].text))
     #   get_sentiment(public_tweets[i].text)
 
-    client.get_old_tweets()
+    search_words = ["#tesla", "#stock"]
 
-    # for i in range(2):
-    #   print("sentiment score: ", get_sentiment(public_tweets[i]), public_tweets[i])
+    date_since_arr = ["201610310000", "201611050000"]
+    date_until_arr = ["201611020000", "201611060000"]
+    #client.get_old_tweets(date_since_arr[0], date_until_arr[0], search_words[0])
+
+
+    # Retrieve past tweets in parallel
+    #with Pool(5) as p:
+       #p.starmap(client.get_old_tweets, zip(
+            #date_since_arr, date_until_arr, repeat(search_words[0]), repeat(search_words[1])))
+
+    client.get_old_tweets(date_since_arr[0], date_until_arr[0], search_words[0])
+
+
+    # Reads in files with past tweets and extracts out text from each
+    queried_tweets = []
+
+    with open('past_tweets_201610310000_201611020000.txt', 'r') as file:
+        for line in file:
+            obj = json.loads(line)
+            queried_tweets.append(obj['text'])
+
+    sentiments = []
+    for tweet in queried_tweets:
+        sentiments.append(client.get_sentiment(client.clean(tweet)))
+    
+    print(sentiments)
+
+
+    tsla = yf.Ticker("TSLA")
+    tsla_stock = tsla.history(
+        start=(date_since_arr[0].strftime('%Y-%m-%d')),
+        end=date_until_arr[0].strftime('%Y-%m-%d'),
+        interval='60m'
+    ).reset_index()
+
+
+
+
+
+    
+    
+
+    
